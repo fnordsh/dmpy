d5fdce7915f603f8bd208ecf979eec232eddace4
Gert Menke <gert@menke.ac>
2020-05-05 21:59:42 +0200

    Improve file reader; closes #1

--- src/reader.c
+++ src/reader.c
@@ -7,66 +7,69 @@
 
 mp_lexer_t *mp_lexer_new_from_file(const char *filename) {
     mp_reader_t reader;
-    // mp_reader_new_file(&reader, filename);
-
-    // use a mem reader for testing
-    FIL f;
-    MP_THREAD_GIL_EXIT();
-    FRESULT fr = f_open(&f, filename, FA_READ);
-    MP_THREAD_GIL_ENTER();
-    if(fr) {
-        mp_raise_OSError(errno);
-    } else {
-        FSIZE_t fsize = f_size(&f);
-        byte *filebuf = malloc(fsize+1);
-        if(filebuf==0) {
-            mp_raise_OSError(errno);
-        } else {
-            unsigned int read_bytes;
-            f_read(&f, filebuf, fsize, &read_bytes);
-            f_close(&f);
-            filebuf[fsize]=0;
-            mp_reader_new_mem(&reader, filebuf, fsize, fsize+1);
-        }
-    }
-
+    mp_reader_new_file(&reader, filename);
     return mp_lexer_new(qstr_from_str(filename), reader);
 }
 
-/*
+typedef struct _mp_reader_dmcp_t {
+    FIL f;
+    size_t len;
+    size_t pos;
+    byte buf[20];
+} mp_reader_dmcp_t;
+
 void mp_reader_new_file(mp_reader_t *reader, const char *filename) {
-    lcd_putsRAt(t20, 7, "mp_reader_new_file:");
-    lcd_putsRAt(t20, 8, filename);
+    mp_reader_dmcp_t *rd = m_new_obj(mp_reader_dmcp_t);
+    FRESULT fr;
 
     MP_THREAD_GIL_EXIT();
-    FIL *fp;
-    FRESULT fr = f_open(fp, filename, FA_READ);
+    fr = f_open(&(rd->f), filename, FA_READ);
     MP_THREAD_GIL_ENTER();
-    if (fr != FR_OK) {
-        mp_raise_OSError(errno);
+    if(fr) {
+        mp_raise_OSError(errno); // TODO: Find out why this even compiles and if we can raise a more meaningful exception
+        // does not return
     }
-    //mp_reader_new_file_from_fd(reader, fd, true);
-    reader=0;
+
+    fr = f_read(&(rd->f), &(rd->buf), sizeof(rd->buf), &(rd->len));
+    if(fr) {
+        mp_raise_OSError(errno); // TODO: Find out why this even compiles and if we can raise a more meaningful exception
+        // does not return
+    }
+
+    reader->data = rd;
+    reader->readbyte = mp_reader_dmcp_readbyte;
+    reader->close = mp_reader_dmcp_close;
 }
 
-void mp_reader_new_file_from_fd(mp_reader_t *reader, int fd, bool close_fd) {
-    mp_reader_posix_t *rp = m_new_obj(mp_reader_posix_t);
-    rp->close_fd = close_fd;
-    rp->fd = fd;
-    MP_THREAD_GIL_EXIT();
-    int n = read(rp->fd, rp->buf, sizeof(rp->buf));
-    if (n == -1) {
-        if (close_fd) {
-            close(fd);
+STATIC mp_uint_t mp_reader_dmcp_readbyte(void *data) {
+    mp_reader_dmcp_t *rd = (mp_reader_dmcp_t *)data;
+    FRESULT fr;
+
+    if (rd->pos >= rd->len) {
+        if (rd->len == 0) {
+            return MP_READER_EOF;
+        } else {
+            MP_THREAD_GIL_EXIT();
+            fr = f_read(&(rd->f), &(rd->buf), sizeof(rd->buf), &(rd->len));
+            MP_THREAD_GIL_ENTER();
+            if(fr) {
+                mp_raise_OSError(errno); // TODO: Find out why this even compiles and if we can raise a more meaningful exception
+                // does not return
+            }
+            if (rd->len <= 0) {
+                rd->len = 0;
+                return MP_READER_EOF;
+            }
+            rd->pos = 0;
         }
-        MP_THREAD_GIL_ENTER();
-        mp_raise_OSError(errno);
     }
-    MP_THREAD_GIL_ENTER();
-    rp->len = n;
-    rp->pos = 0;
-    reader->data = rp;
-    reader->readbyte = mp_reader_posix_readbyte;
-    reader->close = mp_reader_posix_close;
+    return rd->buf[rd->pos++];
 }
-*/
+
+STATIC void mp_reader_dmcp_close(void *data) {
+    mp_reader_dmcp_t *rd = (mp_reader_dmcp_t *)data;
+    MP_THREAD_GIL_EXIT();
+    f_close(&(rd->f));
+    MP_THREAD_GIL_ENTER();
+    m_del_obj(mp_reader_dmcp_t, rd);
+}
--- src/reader.h
+++ src/reader.h
@@ -3,5 +3,5 @@
 mp_lexer_t *mp_lexer_new_from_file(const char *filename);
 
 void mp_reader_new_file(mp_reader_t *reader, const char *filename);
-
-
+STATIC mp_uint_t mp_reader_dmcp_readbyte(void *data);
+STATIC void mp_reader_dmcp_close(void *data);
